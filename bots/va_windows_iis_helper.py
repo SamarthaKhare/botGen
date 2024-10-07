896b3cbdc26c46c6a33216e859419ed5944f77df
Samartha Khare <97874411+SamarthaKhare@users.noreply.github.com>
2024-10-07 18:17:19 +0530

    Update va_windows_iis_helper.py
    added doc-string

--- bots/va_windows_iis_helper.py
+++ bots/va_windows_iis_helper.py
@@ -1,10 +1,18 @@
 import os
-'''
+
 def check_default_website_presence(host_name, is_ntlm=None):
     """
-    This function check the default status of iis 
-    Args: host_name
-    Returns : A string either "true" or "false"  
+    Checks the presence or status of the default IIS (Internet Information Services) website on a remote server.
+
+    Args:
+        host_name (str): The hostname or IP address of the remote server.
+        is_ntlm (bool, optional): Indicates whether NTLM authentication is required. Defaults to None.
+
+    Returns:
+        bool: True if the default IIS website is enabled, False if it is disabled or an error occurs.
+
+    Example:
+        check_default_website_presence("192.168.1.15")
     """
     from remote_connection_helper import get_winrm_result
     try:
@@ -16,18 +24,25 @@ def check_default_website_presence(host_name, is_ntlm=None):
     except Exception as exception:
         print(exception)
         return False
-'''
-'''
+        
 def disable_default_document(host_name, is_ntlm=None):
     """
-    This function disables the default document 
-    Args: host_name
-    Returns : A string either 'IIS default document disabled successfully' or 'Error disabling IIS default document'
+    Disables the default document setting in IIS (Internet Information Services) for a remote server.
+
+    Args:
+        host_name (str): The hostname or IP address of the remote server.
+        is_ntlm (bool, optional): Indicates whether NTLM authentication is required. Defaults to None.
+
+    Returns:
+        str: A message indicating whether the default IIS document was successfully disabled or an error occurred.
+
+    Example:
+        disable_default_document("192.168.1.15")
     """
     from remote_connection_helper import get_winrm_result
     try:
         command = f"""
-        onProperty -filter 'system.webserver/defaultdocument' -pspath 'IIS:\\sites\\Default Web Site' -name 'enabled' -Value 'False'
+        Set-WebConfigurationProperty -filter 'system.webserver/defaultdocument' -pspath 'IIS:\\sites\\Default Web Site' -name 'enabled' -Value 'False'
         if ($?) {{
             echo 'True'
         }} else {{
@@ -42,13 +57,20 @@ def disable_default_document(host_name, is_ntlm=None):
     except Exception as exception:
         print(exception)
         return 'Error disabling IIS default document'
-'''
-'''
+
 def enable_default_document(host_name, is_ntlm=None):
     """
-    This function enables the default document  
-    Args: host_name
-    Returns : A string either 'IIS default document enabled successfully' or 'Error enabling IIS default document' 
+    Enables the default document setting in IIS (Internet Information Services) for a remote server.
+
+    Args:
+        host_name (str): The hostname or IP address of the remote server.
+        is_ntlm (bool, optional): Indicates whether NTLM authentication is required. Defaults to None.
+
+    Returns:
+        str: A message indicating whether the default IIS document was successfully enabled or an error occurred.
+
+    Example:
+        enable_default_document("192.168.1.15")
     """
     from remote_connection_helper import get_winrm_result
     try:
@@ -68,4 +90,6 @@ def enable_default_document(host_name, is_ntlm=None):
     except Exception as exception:
         print(exception)
         return 'Error enabling IIS default document'
-'''
+
+
+
