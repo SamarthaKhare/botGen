76aba09d8b9793bff542aafc3296b960f6715e07
Samartha Khare <97874411+SamarthaKhare@users.noreply.github.com>
2024-10-17 13:13:35 +0530

    Update utility_helper.py
    updated env variables

--- bots/utility_helper.py
+++ bots/utility_helper.py
@@ -1,3 +1,13 @@
+import logging
+import requests
+import os
+from requests.auth import HTTPBasicAuth
+import json
+from dotenv import load_dotenv
+dir=os.path.dirname(os.path.abspath(__file__))
+dotenv_path = f"{dir}/../../.env"
+load_dotenv(dotenv_path=dotenv_path)
+
 def remove_spaces(value):
     """
     Recursively removes leading and trailing spaces from a given value, which can be a string, list, or dictionary.
@@ -20,36 +30,23 @@ def remove_spaces(value):
     else:
         return value
 
-def create_ticket(element):
-    """
-    Creates an incident ticket in ServiceNow using the provided element as the short description and description.
-
-    This function sends a POST request to the ServiceNow API to create a new incident ticket. The `element` parameter 
-    is used both for the short description and the detailed description of the ticket. The function uses basic 
-    authentication and sends the request in JSON format. Upon success, the function logs the response and returns the 
-    response JSON. If an error occurs, it logs the error and returns an error message in JSON format.
-
-    Args:
-        element (str): The description of the incident to be used for both 'short_description' and 'description' fields.
-
-    Returns:
-        dict: The JSON response from the ServiceNow API if the request is successful, or an error message if the request fails.
-    """
-    instance = ''  
-    username = ''  
-    password = ''  
+def create_ticket(message):
+ 
+    instance = os.environ['SN_INSTANCE']
+    username = os.environ['SN_USERNAME']
+    password = os.environ['SN_PASSWORD']
     url = f'https://{instance}.service-now.com/api/now/table/incident'
     headers = {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
     }
     data = {
-        'short_description': element,
-        'description': element
+        'short_description': message,
+        'description': message,
+        'category':'inquiry'
     }
     
     logging.info(f"Sending request to ServiceNow with data: {data}")
-    
     try:
         response = requests.post(url, auth=HTTPBasicAuth(username, password), headers=headers, data=json.dumps(data))
         response.raise_for_status()  
