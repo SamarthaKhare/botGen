66ff35fc6575a9d3a1c6f13497878fb7ab276fc1
Samartha Khare <97874411+SamarthaKhare@users.noreply.github.com>
2024-11-16 17:40:06 +0530

    Update servicenow.py

--- bots/servicenow.py
+++ bots/servicenow.py
@@ -2,10 +2,14 @@ import requests
 import json
 import os
 import sys
-from zif_workflow_helper import get_workflow_config_value 
+from requests.auth import HTTPBasicAuth
+from dotenv import load_dotenv
+dir=os.path.dirname(os.path.abspath(__file__))
+dotenv_path = f"{dir}/.env"
+load_dotenv(dotenv_path=dotenv_path)
 
-headers = {"Content-Type": "application/json"}
-
+headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+authentication = HTTPBasicAuth(os.getenv('SN_USERNAME'), os.getenv('SN_PASSWORD'))
 def update_incident(sys_id, payload):
 	"""	
 	This function updates an incident in ServiceNow using the provided system ID and payload.
@@ -20,15 +24,8 @@ def update_incident(sys_id, payload):
 		#endpoint = get_workflow_config_value("UNICONN_EXPORT_ENDPOINT")
 		endpoint=f"/api/now/v2/table/incident/{sys_id}"
 		path = "{}{}".format(base_resource, endpoint)
-		request_payload = {
-			"operation": "REMEDIATE_ITSM",
-			"exportedTool": "ServiceNow",
-			"payload": {
-				"refId": sys_id,
-				"request": str(json.dumps(payload))
-			}
-		}
-		response = requests.request("POST",path, headers=headers, data=json.dumps(request_payload))
+		print(path)
+		response = requests.request("PATCH",path, headers=headers, data=json.dumps(payload),auth=authentication)
 		if response is not None and response.status_code == 200:
 			result = "SUCCESS"
 	except Exception as exception:
