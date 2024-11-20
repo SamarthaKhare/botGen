6afc4acb86e4fbd056bb48374b9a0959bc322cb7
Samartha Khare <97874411+SamarthaKhare@users.noreply.github.com>
2024-11-20 14:54:34 +0530

    Update utility_helper.py

--- bots/utility_helper.py
+++ bots/utility_helper.py
@@ -4,10 +4,14 @@ import requests
 import os
 from requests.auth import HTTPBasicAuth
 import json
+from remote_connection_helper import is_ping_success,get_winrm_reachable_status,get_ssh_reachable_status
+from servicenow import update_incident
 from dotenv import load_dotenv
+
 dir=os.path.dirname(os.path.abspath(__file__))
 dotenv_path = f"{dir}/../../.env"
 load_dotenv(dotenv_path=dotenv_path)
+from mongo_config import MONGO_CONFIG 
 
 headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
 authentication = HTTPBasicAuth(os.getenv('SN_USERNAME'), os.getenv('SN_PASSWORD'))
@@ -79,3 +83,63 @@ def search_incident(filter_query):
 	except Exception as exception:
 		print(exception)
 	return result
+
+def is_device_reachable(device_config,workflow_name):
+    """
+    Checks the reachability status of a device based on its configuration. It first checks if the 
+    device is reachable via ping, then verifies its accessibility over SSH (for Linux devices) or 
+    WinRM (for non-Linux devices). The function returns the status of the device based on the 
+    reachability checks and if it is unreachable it 
+    Arguments: - device_config (dict): A dictionary containing device configuration details such as device_name,and whether the device is Linux or not.
+               - workflow name 
+    Returns:str: A string indicating the reachability status ("Success", "Ping Failure", "SSH Failure","Winrm Failure").
+    """
+    status = None
+    try: 
+        if device_config is not None: 
+            retry_count = 3
+            if retry_count is None:
+                retry_count = 3
+            if is_ping_success(device_config['device_name'],retry_count):
+                if device_config['is_linux']:
+                    if get_ssh_reachable_status(device_config['device_name']):
+                        status = "Success"
+                        print("Success")
+                    else:
+                        status = "SSH Failure"
+                        print("SSH Failure")
+                else:
+                    if get_winrm_reachable_status(device_config['device_name']) == 'Success':
+                        status = "Success"
+                        print(status)
+                    else:
+                        status = "Winrm Failure"
+                        print(status)
+            else:
+                status="Ping Failure"
+                print("Ping Failure")
+        else:
+            print("Device Config is empty.")
+        if status == "Success":
+            return "Success"
+        else:
+            device_unreachable_status(device_config,status,workflow_name)
+            return status
+    except Exception as exception:
+        print(exception)
+
+def device_unreachable_status(device_config,failureStatus,workflow_name):
+    """
+    """
+    try:
+        config = MONGO_CONFIG[workflow_name]        
+        if all([device_config,failureStatus,config]) and 'ESCALATE_DEVICE_UNREACHABLE' in config:
+            sys_id = device_config['sysId']
+            device_name = device_config['deviceName']
+            incident_payload = config['ESCALATE_DEVICE_UNREACHABLE']['INCIDENT_PAYLOAD']
+            incident_payload['work_notes'] = incident_payload['work_notes'].format(DEVICE_NAME=
+                                                    device_name,FAILURE_TYPE=failureStatus)
+            response = update_incident(sys_id,incident_payload)
+            print('response is',response)
+    except Exception as exception:
+        print(exception)
