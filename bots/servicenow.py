d92cd0cdea47bf33ea08516a0bf49fe9098b75ab
Samartha Khare <97874411+SamarthaKhare@users.noreply.github.com>
2024-11-20 19:10:04 +0530

    Update servicenow.py

--- bots/servicenow.py
+++ bots/servicenow.py
@@ -5,30 +5,59 @@ import sys
 from requests.auth import HTTPBasicAuth
 from dotenv import load_dotenv
 dir=os.path.dirname(os.path.abspath(__file__))
-dotenv_path = f"{dir}/.env"
+dotenv_path = f"{dir}/../../.env"
 load_dotenv(dotenv_path=dotenv_path)
 
+host = f"https://{os.getenv('SN_INSTANCE')}.service-now.com"
+print(host)
 headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
 authentication = HTTPBasicAuth(os.getenv('SN_USERNAME'), os.getenv('SN_PASSWORD'))
-def update_incident(sys_id, payload):
-	"""	
-	This function updates an incident in ServiceNow using the provided system ID and payload.
-	Args:sys_id (str): The system ID of the incident to be updated.
-	     payload (dict): A dictionary containing the data to update the incident with.
-	Returns:str: "SUCCESS" if the update is successful, "ERROR" if an error occurs, or "NO RESULT" if no response is received.
-	"""
-	result = "NO RESULT"
-	try:
-		base_resource = base = f"https://{os.getenv('SN_INSTANCE')}.service-now.com"
-		#endpoint = get_workflow_config_value("UNICONN_EXPORT_ENDPOINT")
-		endpoint=f"/api/now/v2/table/incident/{sys_id}"
-		path = "{}{}".format(base_resource, endpoint)
-		print(path)
-		print(payload)
-		response = requests.request("PATCH",path, headers=headers, data=json.dumps(payload),auth=authentication)
-		if response is not None and response.status_code == 200:
-			result = "SUCCESS"
-	except Exception as exception:
-		print(exception)
-		result = "ERROR"
-	return result
+
+def get_api_response(attributes):
+    response, response_code = None, None
+    try:
+        if attributes is not None:
+            request_type = attributes["type"]
+            request_url = attributes["url"]
+            request_payload = attributes["payload"]
+
+            if all([request_type, request_url]):
+                request_url = "".join([host, request_url])
+                if request_type == "GET" and request_payload is None:
+                    response = requests.request(request_type, request_url, headers=headers, auth=authentication)
+                else:
+                    response = requests.request(request_type, request_url, headers=headers, data=json.dumps(request_payload), auth=authentication)
+        else:
+            print("One or more params are empty")
+        
+        if response is not None:
+            response_code = response.status_code
+            response = response.json()
+    except Exception as exception:
+        error_message = f"Error executing API request: {exception}"
+        print(error_message)
+    return response, response_code
+
+# Function to update the status of an incident
+def update_incident(sys_id,data):
+    response = None
+    try:
+        payload=\
+        {
+            "close_code": data['close_code'],
+            "incident_state": data['state'],
+            "caller_id": "admin",
+            "close_notes": data['close_notes'],
+            'work_notes':data['work_notes']
+		}
+        url = f"/api/now/v2/table/incident/{sys_id}"
+        attributes = {"type": "PATCH", "url": url, "payload": payload}
+        result, response_code = get_api_response(attributes)
+        if response_code == 200:
+            response = "SUCCESS"
+        else:
+            response = result
+    except Exception as exception:
+        error_message = f"Error updating status: {exception}"
+        print(error_message)
+    return response
